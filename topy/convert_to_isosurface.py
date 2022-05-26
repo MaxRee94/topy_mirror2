a426fbbf4fe8869260d459ffbc506be94cf43d1d
Max van der Ree <maxvdree@hotmail.com>
2022-05-26 23:30:06 +0200

    Correct voldata file path

--- topy/convert_to_isosurface.py
+++ topy/convert_to_isosurface.py
@@ -3,14 +3,14 @@ from sys import argv
 import numpy as np
 import mcubes
 
-# voldata_file = argv[1]
-# if not os.path.exists(argv[1]):
-#     print("file", argv[1], "does not exist. Aborting iso conversion.")
+voldata_file = argv[1]
+if not os.path.exists(argv[1]):
+    print("file", argv[1], "does not exist. Aborting iso conversion.")
 
 #X, Y, Z = np.mgrid[:6, :6, :6]
 #u = (X-3)**2 + (Y-3)**2 + (Z-3)**2 - 2**2
 
-voldata_file = r"E:\Development\Generative_Design_Research\topy\iterations\mbb_beam_3d_exp_gsf_050.npy"
+# voldata_file = r"E:\Development\Generative_Design_Research\topy\iterations\mbb_beam_3d_exp_gsf_050.npy"
 
 #print("u:\n", u)
 #exit()
